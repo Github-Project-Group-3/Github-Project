7423d6dfdac040b6047e119f6c9ea8ab66ecbfd0
BryanTurns <95263942+BryanTurns@users.noreply.github.com>
2023-11-15 12:58:44 -0700

    Added personal page for Bryan

---
+++ src/pages/BryanTurns.js
@@ -0,0 +1,10 @@
+export default function BryanTurns() {
+  return (
+    <div>
+      <h1>About Me: Bryan Turns</h1>
+      <p>
+        My name is Bryan and I love to lift, climb, ski, code, and plently more.{" "}
+      </p>
+    </div>
+  );
+}
