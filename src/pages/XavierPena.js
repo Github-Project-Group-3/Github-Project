4ad832bd34e5107148880cbb9873237df7ca10f7
xpena10 <150300920+xpena10@users.noreply.github.com>
2023-11-19 12:38:15 -0700

    Create XavierPena.js

---
+++ src/pages/XavierPena.js
@@ -0,0 +1,13 @@
+export default function XavierPena() {
+    return (
+      <div>
+        <h1>About Me: Xavier Pena</h1>
+        <h3>
+            Hi! My name is Xavier, I'm going into Computer Science as my major with a goal of joining the cybersecurity space! 
+        </h3>
+        <p>
+            I like to listen to most genres of music and play video games.
+        </p>
+      </div>
+    );
+  }
