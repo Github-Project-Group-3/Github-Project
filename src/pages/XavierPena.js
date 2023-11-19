a8c2710587a2b1ae10e401d5104b8cee49cc2fd8
xpena10 <150300920+xpena10@users.noreply.github.com>
2023-11-19 12:45:15 -0700

    Update XavierPena.js

--- src/pages/XavierPena.js
+++ src/pages/XavierPena.js
@@ -1,12 +1,14 @@
 export default function XavierPena() {
     return (
       <div>
-        <h1><center>About Me: <strong>Xavier Pena </strong></center></h1>
+        <h1><center>About Me: <strong>Xavier Pena </strong>
+        <img src="https://media.licdn.com/dms/image/D5603AQH31J6IAR68QQ/profile-displayphoto-shrink_200_200/0/1664330678167?e=1706140800&v=beta&t=h1tlIxgdMRsokYmkYNQdOMw9-6q71uhvp2juKhaqZ_I"></img></center></h1>
         <h3>
             <center>Hi! My name is Xavier, I'm going into Computer Science as my major with a goal of joining the cybersecurity space! </center>
         </h3>
         <p>
         <center>I like to listen to most genres of music and play video games.</center>
+        
         </p>
       </div>
     );
